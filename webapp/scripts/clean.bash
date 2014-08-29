1b841e60c1c9407e936d427c35f7dbe2ab54aff8
David Davidović <geosoft.corp@gmail.com>
2014-08-29 21:04:54 +0200

    Some small changes to the clean.bash script
    To delete everything more thoroughly, I had some problems with leftover
    build files

--- webapp/scripts/clean.bash
+++ webapp/scripts/clean.bash
@@ -2,6 +2,6 @@
 for dir 
 do
     echo "Cleaning $dir"
-    rm -rf $dir/js/*.js $dir/css/*.css $dir/images/* $dir/fonts/* $dir/html/*.html
+    rm -rf $dir/js/* $dir/css/* $dir/images/* $dir/fonts/* $dir/*.html
 done
 
